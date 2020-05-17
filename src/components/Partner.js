f9f0e0a6391fe8d181d4e6307fe1210a2b8554e9
jonasgrunert <jonas@grunert-berlin.de>
2020-05-17 22:26:01 +0200

    Toggling is active

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Img from "gatsby-image";
 
-export default ({ image, title, link, description }) => (
-  <div className="is-picture-wrapper column is-one-third">
-    <figure className="image">
-      <Img alt={title} fluid={image} />
-    </figure>
-    <div className="is-picture-content has-text-white">
-      <h1 className="subtitle has-text-white">{title}</h1>
-      <p>{description}</p>
-      <a href={link}>Webseite</a>
+export default ({ image, title, link, description }) => {
+  const [active, setActive] = useState(false);
+  return (
+    <div
+      className={`is-picture-wrapper column is-one-third ${
+        active ? "is-active" : ""
+      }`}
+      onClick={() => {
+        console.log("hi");
+        setActive(!active);
+      }}
+    >
+      <figure className="image">
+        <Img alt={title} fluid={image} />
+      </figure>
+      <div className="is-picture-content has-text-white">
+        <h1 className="subtitle has-text-white">{title}</h1>
+        <p>{description}</p>
+        <a href={link}>Webseite</a>
+      </div>
     </div>
-  </div>
-);
+  );
+};
